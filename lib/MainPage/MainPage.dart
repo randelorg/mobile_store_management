4f4a48a809108fec177ff279caf53ee929d1f36e
michaeljudejacinto <michaeljude.jacinto@unc.edu.ph>
2021-09-08 12:21:11 +0800

    mainpage UI (#1)

---
+++ lib/MainPage/MainPage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../MainPage/MainPage.dart';
+import '../MainPage/Home.dart';
+
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  
+  List<Widget> _widgetOptions = <Widget>[
+    Home(),
+     
+   
+  ];
+ 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('test'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            title: Text('test'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+ 
