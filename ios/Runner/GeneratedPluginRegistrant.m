80b870a53d3e28525e35bc97020ffb43ee6c37c5
michaeljudejacinto <michaeljude.jacinto@unc.edu.ph>
2021-09-16 00:09:08 +0800

    Main UI

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -2,7 +2,15 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_barcode_scanner/SwiftFlutterBarcodeScannerPlugin.h>)
+#import <flutter_barcode_scanner/SwiftFlutterBarcodeScannerPlugin.h>
+#else
+@import flutter_barcode_scanner;
+#endif
 
 #if __has_include(<path_provider/FLTPathProviderPlugin.h>)
 #import <path_provider/FLTPathProviderPlugin.h>
@@ -13,6 +21,7 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SwiftFlutterBarcodeScannerPlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftFlutterBarcodeScannerPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
--- lib/MainPage/MainPage.dart
+++ lib/MainPage/MainPage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../MainPage/MainPage.dart';
 import '../MainPage/Home.dart';
-
 
 class MainPage extends StatefulWidget {
   @override
@@ -10,47 +8,139 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int _selectedIndex = 0;
-  
-  List<Widget> _widgetOptions = <Widget>[
+  int currentTab = 0; // to keep track of active tab index
+
+  final List<Widget> screens = [
     Home(),
-     
-   
-  ];
- 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
- 
+  ]; // to store nested tabs
+
+  Widget currentScreen = Home(); // Our first view in viewport
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: currentScreen,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('test'),
+      floatingActionButton: FloatingActionButton(
+
+        child: Icon(Icons.qr_code_scanner),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        hoverColor: Colors.black,
+
+        onPressed: () {
+          setState(() {
+            currentScreen =
+                Home(); // if user taps on this dashboard tab will be active
+            currentTab = 0;
+          });    
+        }, 
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 75,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen =
+                            Home(); // if user taps on this dashboard tab will be active
+                        currentTab = 1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.payment,
+                          color: currentTab == 1 ? Colors.red : Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    minWidth: 75,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen =
+                             Home(); // if user taps on this dashboard tab will be active
+                        currentTab = 2;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.chat,
+                          color: currentTab == 2 ? Colors.red : Colors.black,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              // Right Tab bar icons
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 75,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen =
+                            Home(); // if user taps on this dashboard tab will be active
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.dashboard,
+                          color: currentTab == 3 ? Colors.red : Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Settings
+                  MaterialButton(
+                    minWidth: 75,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen =
+                            Home(); // if user taps on this settings tab will be active
+                        currentTab = 4;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.settings,
+                          color: currentTab == 4 ? Colors.red : Colors.black,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark),
-            title: Text('test'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Settings'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        ),
       ),
     );
   }
 }
- 
